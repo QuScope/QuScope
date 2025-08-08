d255110914a13f4a2dd776294daa8b440000c8de
Sean D. Lam <144201405+seanthelam@users.noreply.github.com>
2025-08-08 16:23:42 -0500

    Update __init__.py

--- src/quscope/eels_analysis/__init__.py
+++ src/quscope/eels_analysis/__init__.py
@@ -1,10 +1,19 @@
 """Quantum EELS (Electron Energy Loss Spectroscopy) analysis module."""
 
-from .preprocessing import preprocess_eels_data
-from .quantum_processing import create_eels_circuit, analyze_eels_spectrum
+from quscope.eels_analysis.preprocessing import preprocess_eels_data, extract_eels_features
+from quscope.eels_analysis.quantum_processing import QuantumCircuitLibrary, QuantumPreprocessor, QuantumFeatureExtractor, QuantumMLProcessor
+from quscope.eels_analysis.eels_utils import ElementSubstitutionEngine, SpatialMappingEngine
+from quscope.eels_analysis.analysis import EELSAnalyzer
 
+__version__ = "0.1.0"
 __all__ = [
-    'preprocess_eels_data',
-    'create_eels_circuit',
-    'analyze_eels_spectrum'
+    preprocess_eels_data,
+    extract_eels_features,
+    QuantumCircuitLibrary,
+    QuantumPreprocessor,
+    QuantumFeatureExtractor,
+    QuantumMLProcessor,
+    ElementSubstitutionEngine,
+    SpatialMappingEngine,
+    EELSAnalyzer
 ]
